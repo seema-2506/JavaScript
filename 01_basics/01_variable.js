dd7e445637472e6b3d31ae497031b4fdba436a49
Seema_2506 <seemaararia@gmail.com>
2025-05-30 07:25:39 +0000

    changes made done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accoutId = 154524
+let accountEmail = "seema@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+let accountState;
+//accoutId= 2 // not allowed
+
+accountEmail = "sam@gmail.com"
+accountPassword = "2777463476"
+accountCity = "patna"
+console.log(accoutId);
+console.table(["AccountId: "+accoutId,"accountEmail: "+ accountEmail,"accountPassword: "+ accountPassword,"accountCity: "+ accountCity,"accountState: "+ accountState])
