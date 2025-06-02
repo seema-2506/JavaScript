fb3c984a19f49349819f481a15b4445b01bb62d3
Seema_2506 <seemaararia@gmail.com>
2025-06-02 18:56:50 +0000

    conveersion of datatypes

---
+++ 01_basics/03_convertorOperations.js
@@ -0,0 +1,24 @@
+// let score =  "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc=>NaN"
+// true => 1; false => 0
+
+let isLoggedIn= 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
