4a17279aa4e69e6c9b53601d93764a620f7d4c54
Seema_2506 <seemaararia@gmail.com>
2025-06-02 18:43:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert(3+3) // we are using Node.js, not browser
+
+
+// console.log(3+
+//     3)  // code readability should be high
+console.log("seema");
+
+
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2^53
+//bigint
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+//symbol=> unique
+
+//object
+
+console.log(typeof "seema")
+console.log(typeof undefined)
+console.log(typeof null)
