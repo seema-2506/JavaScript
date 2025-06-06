c0510a9d6d11127dd0947e5cb9c09f2dc09a3c86
Seema_2506 <seemaararia@gmail.com>
2025-06-06 09:25:48 +0000

    changes done

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,32 @@
+// const name = "Seema"
+
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value");  //outdated syntax
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);   // String interpolation
+
+
+const gameName = new String("Seema-sr")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.substring(1, 4));
+
+// const anotherString = gameName.slice(-8, 4)  // will start reversely
+// console.log(anotherString);
+
+const newStringOne = "  Seema  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://seema.com/seema%20rani"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('jdjc'));
+
+console.log(gameName.split('-'));
+
+
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -3,57 +3,77 @@
 // 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
 //JavaScript is dynamically typed language
-const score = 100 
-const scoreValue = 100.3
+// const score = 100 
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
+// const isLoggedIn = false
+// const outsideTemp = null
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 234567899876543n
-
-
-
-// Reference (Non primitive) Types:
-
-// Array, Onjects, Functions
+// const bigNumber = 234567899876543n
 
 
-const heros = ["shaktiman", "naagraj", "doga"]
-let myObj = {
-    name: "Seema",
-    age: 23,
+
+// // Reference (Non primitive) Types:
+
+// // Array, Onjects, Functions
+
+
+// const heros = ["shaktiman", "naagraj", "doga"]
+// let myObj = {
+//     name: "Seema",
+//     age: 23,
+// }
+
+// const myFunction = function (){
+//     console.log("Hello World!");    
+// }
+
+// console.log(typeof outsideTemp);
+
+// console.log(typeof anotherId); //Symbol
+
+// console.log(typeof bigNumber); //bigint
+
+// console.log(typeof heros); //object
+
+// console.log(typeof isLoggedIn); // boolean
+
+// console.log(typeof myFunction); //function
+
+// console.log(typeof myObj); //object
+// console.log(typeof score); //number 
+
+// console.log(typeof scoreValue); //number
+// console.log(typeof userEmail); //undefined
+
+
+// *************************************************************
+
+// Stack (Primitive), Heap (Non Primitive)
+
+let myName = "Seema Rani"
+
+let anotherName = myName
+anotherName = "Rohan"
+
+console.log(myName);
+console.log(anotherName );
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function (){
-    console.log("Hello World!");    
-}
-
-console.log(typeof outsideTemp);
-
-console.log(typeof anotherId); //Symbol
-
-console.log(typeof bigNumber); //bigint
-
-console.log(typeof heros); //object
-
-console.log(typeof isLoggedIn); // boolean
-
-console.log(typeof myFunction); //function
-
-console.log(typeof myObj); //object
-console.log(typeof score); //number 
-
-console.log(typeof scoreValue); //number
-console.log(typeof userEmail); //undefined
+let userTwo = userOne
+userTwo.email = "seema@gmail.com"
 
 
-
-
-
+console.log(userOne.email);
+console.log(userTwo.email);
