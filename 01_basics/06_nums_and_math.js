7727af00f3c4f19c695aa062af676790ccb5b64c
Seema_2506 <seemaararia@gmail.com>
2025-06-06 10:11:31 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,55 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// const balance1 = balance.toString()
+// console.log(balance1.length);
+// console.log(typeof balance1);
+
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 1123.2687
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//console.log(Number.MAX_VALUE)
+
+
+//+++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.ceil(4.4));
+
+// console.log(Math.floor(4.7));
+
+// console.log(Math.min(3,4,2,1,-3));
+
+// console.log(Math.max(4,5,6,3,2));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
