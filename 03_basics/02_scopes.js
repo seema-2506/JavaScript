c0130ff26a1268de98c1f43edc16e8a86050ea61
Seema_2506 <seemaararia@gmail.com>
2025-06-07 11:53:26 +0000

    functions and fuction expressions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,18 +1,69 @@
 // global scope
 //var c = 300
-let a = 400
+// let a = 400
 
-// block scope
-if(true){
-    let a = 20
-    const b = 30
-   console.log("Inner: ",a);
-}
+// // block scope
+// if(true){
+//     let a = 20
+//     const b = 30
+//    //console.log("Inner: ",a);
+// }
 
 // for(let i = 0;i < array.length; i++){
 //     const element = array[i];
 // }
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 //console.log(c);
+
+
+function one(){
+    const  username = "Seema"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    //console.log(website);
+
+    //two()
+}
+
+//one()
+
+if(true){
+    const username = "seema"
+    if(username === "seema"){
+        const website = " youtube1"
+        //console.log(username + website);
+    }
+    //console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++
+
+// Hoisting
+
+// hoisting possible
+console.log(addone(4));
+
+function addone(num){
+    return num + 1
+}
+
+
+// console.log(addTwo(5)); // hoisting not possible
+
+
+// function expression
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
+
+
